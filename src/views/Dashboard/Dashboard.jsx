d1bb2a257488d7034d65af5b1fb56ffff518d5e8
Home <Home@Homes-MacBook-Air.local>
2017-11-20 00:15:45 +0700

    2

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -42,8 +42,8 @@ class Dashboard extends Component {
                         <Col lg={3} sm={6}>
                             <StatsCard
                                 bigIcon={<i className="pe-7s-server text-warning"></i>}
-                                statsText="Capacity"
-                                statsValue="105GB"
+                                statsText="Trip"
+                                statsValue="105"
                                 statsIcon={<i className="fa fa-refresh"></i>}
                                 statsIconText="Updated now"
                             />
@@ -51,7 +51,7 @@ class Dashboard extends Component {
                         <Col lg={3} sm={6}>
                             <StatsCard
                                 bigIcon={<i className="pe-7s-wallet text-success"></i>}
-                                statsText="Revenue"
+                                statsText="INCOME"
                                 statsValue="$1,345"
                                 statsIcon={<i className="fa fa-calendar-o"></i>}
                                 statsIconText="Last day"
@@ -59,22 +59,22 @@ class Dashboard extends Component {
                         </Col>
                         <Col lg={3} sm={6}>
                             <StatsCard
-                                bigIcon={<i className="pe-7s-graph1 text-danger"></i>}
-                                statsText="Errors"
+                                bigIcon={<i className="pe-7s-star text-danger"></i>}
+                                statsText="Followers"
                                 statsValue="23"
                                 statsIcon={<i className="fa fa-clock-o"></i>}
                                 statsIconText="In the last hour"
                             />
                         </Col>
-                        <Col lg={3} sm={6}>
-                            <StatsCard
-                                bigIcon={<i className="fa fa-twitter text-info"></i>}
-                                statsText="Followers"
-                                statsValue="+45"
-                                statsIcon={<i className="fa fa-refresh"></i>}
-                                statsIconText="Updated now"
-                            />
-                        </Col>
+                        {/*<Col lg={3} sm={6}>*/}
+                            {/*<StatsCard*/}
+                                {/*bigIcon={<i className="fa fa-twitter text-info"></i>}*/}
+                                {/*statsText="Followers"*/}
+                                {/*statsValue="+45"*/}
+                                {/*statsIcon={<i className="fa fa-refresh"></i>}*/}
+                                {/*statsIconText="Updated now"*/}
+                            {/*/>*/}
+                        {/*</Col>*/}
                     </Row>
                     <Row>
                         <Col md={8}>
